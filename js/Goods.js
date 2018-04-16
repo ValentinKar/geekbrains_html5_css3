0b49ff2e7db59f5b4c0637d6de8878017c273011
ValentinKar <mart2013@yandex.ru>
2018-04-16 18:48:57 +0700

    в файле Goods.js добавлен метод onPageSinglePage, который будет отображать большую картинку товара, цену название и описание товара на странице single-page.html

--- js/Goods.js
+++ js/Goods.js
@@ -3,9 +3,14 @@ function Goods(pageNumber, idCategory) {
     this.pageNumber = pageNumber;
     this.idCategory = idCategory;
     this.goodsItems = [];
+    this.goodId = 123;
     this.imgCatalog = 'img/products/';
     this.imgProductCatalog = 'img/Product/';
     this.imgSingleCatalog = 'img/SinglePage/';
+    this.singleImageCatal = 'img/single-img/';
+    this.catalogData = './responses/catalogData.json';
+    this.getGoodById = './responses/getGoodById.json';
+    this.dataType = 'json';
 }
 
 
@@ -13,16 +18,15 @@ Goods.prototype.getGoods = function () {
 
     $.ajax({
         type: 'POST',
-        url: './responses/catalogData.json',
-        dataType: 'json',
+        url: this.catalogData,
+        dataType: this.dataType,
         // отправка id пользователя на сервер
         data: { number: this.pageNumber, id: this.idCategory },
-        // data: { name: "John", location: "Boston" },
         context: this,
         success: function (data) {
             this.onPageIndex(data);
             this.onPageProduct(data);
-            this.onPageSinglePage(data);
+            this.onPageSinglePage(data, this.goodId);
         }
     });
 };
@@ -116,16 +120,66 @@ Goods.prototype.onPageProduct = function (arrayOfGoods) {
 }
 
 
-Goods.prototype.onPageSinglePage = function (arrayOfGoods) {
+Goods.prototype.onPageSinglePage = function (arrayOfGoods, id) {
+
+    $.ajax({
+        type: 'POST',
+        url: this.getGoodById,
+        dataType: this.dataType,
+        // отправка id товара на сервер
+        data: { idOfGood: id },
+        context: this,
+        success: function (data) {
+            if (data.result == 1) {
+                this.onGoodById(data, id);
+            }
+        }
+    });
+
     let catalogOfImage = this.imgSingleCatalog;
     $ftrdItmsDiv = $('.single .fetured_items');
     if ($ftrdItmsDiv.length > 0) {
-    // $ftrdItmsDiv = $('.fetured_items');
-    // $ftrdItmsDiv.empty();
-
         for (let i = 0; i < 4; i++) {
             let $ftrdFigure = new ProductPicture(arrayOfGoods[i], catalogOfImage, true);
             $ftrdItmsDiv.append($ftrdFigure.getWithButtons());
         }
     }
 }
+
+
+Goods.prototype.onGoodById = function (good, id) {
+
+    let singleImageCatal = this.singleImageCatal;
+    $singleImageDiv = $('.single-img');
+    if ($singleImageDiv.length > 0) {
+        $singleImageDiv.append($('<img />', {
+            src: singleImageCatal + id + '.jpg',
+            alt: ''
+        }));
+    }
+
+    let $singleDataForm = $('.single-data');
+    if ($singleDataForm.length > 0) {
+        $singleDataH3 = $('.single-data h3');
+        $singleDataH3.append(good.product_name);
+
+        $singleDataDescriptionP = $('.single-data .container-form');
+        $singleDataDescriptionP.append(good.product_description);
+
+        $singleDataPriceP = $('.single-data .price');
+        $singleDataPriceP.append('$' + good.product_price);
+
+        // кнопка "ADD TO CART" под картинкой товара
+        $singleButton = $('.single-data .white_button');
+            let $addBscktA = $('<a />', {
+                href: '#',
+                class: 'add_basket',
+                'good-id': id
+            }).append($('<i />', {
+                class: 'fa fa-shopping-cart',
+                'aria-hidden': 'true'
+            })).append(' &nbsp; Add to Cart');
+        $singleButton.append($addBscktA);
+    }
+}
+
